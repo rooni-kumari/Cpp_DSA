e26915361cd407084989c0ad7b11e9e9917f85f2
rooni-kumari <kumarirooni44@gmail.com>
2026-02-06 13:55:35 +0530

    multiplication

---
+++ multi.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[5]={1,2,3,4,5},sum=1;
+    for(int num : arr)
+    {
+        sum=sum * num;
+    }
+    cout<<sum<<endl;
+    return 0;
+}
