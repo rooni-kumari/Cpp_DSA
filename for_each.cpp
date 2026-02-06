e2ae32778b4b07042f00f7b141113fe71f29e540
rooni-kumari <kumarirooni44@gmail.com>
2026-02-06 13:50:09 +0530

    for each

---
+++ for_each.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string car[5]={"tata","BMW","swift","name","let"};
+    // cout<<car[0]<<endl;
+    // for(int i=0;i<5;i++)
+    //{
+      //  cout<<car[i]<<" ";
+    //}
+
+    for(string num : car)
+    {
+        cout<<num<<" ";
+    }
+    return 0;
+}
---
+++ sum.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[5]={1,2,3,4,5},sum=0;
+    for(int i=0;i<5;i++)
+    {
+        sum=sum+arr[i];
+    }
+    cout<<sum<<endl;
+    return 0;
+}
