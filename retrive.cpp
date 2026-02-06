2f6767c534665e52329d721aa06488c3cdcab0cf
rooni-kumari <kumarirooni44@gmail.com>
2026-02-06 13:36:47 +0530

    retrive arr

---
+++ retrive.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string car[5]={"tata","BMW","swift","name","let"};
+    cout<<car[4]<<endl;
+    for(int i=0;i<5;i++)
+    {
+        cout<<car[i]<<" ";
+    }
+    return 0;
+}
