377fc8b1d9ee64b8bd2284bf4b8409637c447411
rooni-kumari <kumarirooni44@gmail.com>
2026-02-11 23:44:38 +0530

    maximum occurance

---
+++ max_occ.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {1, 2, 2, 3, 1, 2};
+    int n = 6;
+
+    int maxCount = 0;
+    int maxElement = arr[0];
+
+    for (int i = 0; i < n; i++) {
+        int count = 0;
+
+        for (int j = 0; j < n; j++) {
+            if (arr[i] == arr[j]) {
+                count++;
+            }
+        }
+
+        if (count > maxCount) {
+            maxCount = count;
+            maxElement = arr[i];
+        }
+    }
+
+    cout << "Maximum occurring element: " << maxElement;
+
+    return 0;
+}
