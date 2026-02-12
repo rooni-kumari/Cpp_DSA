ad98b46057fc4aa3facad27123d28a87955644db
rooni-kumari <kumarirooni44@gmail.com>
2026-02-12 21:09:14 +0530

    search element

---
+++ search_ele.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {4, 7, 1, 9, 3};
+    int n = 5;
+    int key = 9;
+
+    int index = -1;  // default if not found
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == key) {
+            index = i;
+            break;   // stop once found
+        }
+    }
+
+    if (index != -1)
+        cout << "Element found at index: " << index;
+    else
+        cout << "Element not found";
+
+    return 0;
+}
