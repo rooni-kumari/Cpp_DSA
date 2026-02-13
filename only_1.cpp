fd5c5d4619885bf95139e12c6b728154387c19b3
rooni-kumari <kumarirooni44@gmail.com>
2026-02-13 14:24:21 +0530

    pattern only 1111

---
+++ only_1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    for(int i=5;i>0;i--)
+    {
+        for(int j=5;j>0;j--)
+        {
+            cout<<"1 ";
+            
+        }
+        cout<<endl;
+    }
+    return 0;
+}
