8ed9e0020a28854f7c22096f019d486c846d751b
rooni-kumari <kumarirooni44@gmail.com>
2026-02-20 23:06:23 +0530

    longest prefix

---
+++ prefix.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+string longestCommonPrefix(vector<string>& strs) {
+    if (strs.size() == 0)
+        return "";
+
+    string prefix = "";
+
+    for (int i = 0; i < strs[0].length(); i++) {
+        char currentChar = strs[0][i];
+
+        for (int j = 1; j < strs.size(); j++) {
+            if (i >= strs[j].length() || strs[j][i] != currentChar) {
+                return prefix;
+            }
+        }
+
+        prefix += currentChar;
+    }
+
+    return prefix;
+}
+
+int main() {
+    vector<string> strs = {"flower", "flow", "flight"};
+
+    cout << "Longest Common Prefix: "
+         << longestCommonPrefix(strs);
+
+    return 0;
+}
